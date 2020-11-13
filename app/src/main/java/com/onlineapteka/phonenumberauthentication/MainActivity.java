7d5f33100d703aee98448672a7e60cc136db4875
rustamkeneev <dr.rustamkeneev@gmail.com>
2020-11-13 23:35:59 +0600

    start project

---
+++ app/src/main/java/com/onlineapteka/phonenumberauthentication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.onlineapteka.phonenumberauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/onlineapteka/phonenumberauthentication/OptActivity.java
@@ -0,0 +1,14 @@
+package com.onlineapteka.phonenumberauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opt);
+    }
+}
